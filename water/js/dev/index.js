d0e5e27c701f31b6d20f7d40948b73e3fafcadef
alyonakrivonos35 <78234483+alyonakrivonos35@users.noreply.github.com>
2025-12-06 16:59:40 +0200

    Add files via upload

--- water/js/dev/index.js
+++ water/js/dev/index.js
@@ -5788,7 +5788,7 @@ function menuInit() {
       document.documentElement.toggleAttribute("data-fls-menu-open");
       return;
     }
-    const menuItem = e.target.closest(".menu__subitem");
+    const menuItem = e.target.closest(".menu__subitem, .menu__sublink");
     if (menuItem && bodyLockStatus) {
       document.documentElement.removeAttribute("data-fls-menu-open");
       bodyLockToggle();
